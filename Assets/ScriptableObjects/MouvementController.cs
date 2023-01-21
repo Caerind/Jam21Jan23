bfa7b01db03ac03e4cd25382599556bbc5c2d16b
alexisbrochec <38135301+alexisbrochec@users.noreply.github.com>
2023-01-21 17:36:14 +0100

    Ajout Animation
    Animation

--- Assets/ScriptableObjects/MouvementController.cs
+++ Assets/ScriptableObjects/MouvementController.cs
@@ -22,23 +22,38 @@ public class MouvementController : MonoBehaviour
 
     bool HasMoved;
 
+    //animation
+    protected Animator animator;
+    protected int animIDMvt;
+
+
     // Update is called once per frame
     void Update()
     {
         GetMouvementDirection();
     }
 
+    protected void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        animIDMvt = Animator.StringToHash("mvt");
+    }
 
-        public void GetMouvementDirection()
+    public void GetMouvementDirection()
     {
         Vector2 p = new Vector2(XVoulu, YVoulu);
             Vector2 delta = p - transform.position.ToVector2();
+        float mvt = delta.magnitude;
+
         delta.Normalize();
+
+        animator?.SetFloat(animIDMvt, mvt);
 
         if ((delta.x < -EcartVoulu || delta.x > EcartVoulu) || (delta.y < -EcartVoulu || delta.y > EcartVoulu))
         {
             float s = Vitesse * Time.deltaTime;
             transform.position += new Vector3(s * delta.x, s * delta.y, 0.0f);
+            //Animation
         }
     }
 
